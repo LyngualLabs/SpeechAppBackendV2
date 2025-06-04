511dcc977a27a2f7a403a55d02afda6f5e252e58
RhythmicRhythm <okubadejorh@gmail.com>
2025-06-05 00:18:58 +0100

    Refactor CORS configuration to enhance security and maintainability

--- src/server.ts
+++ src/server.ts
@@ -15,8 +15,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// CORS configuration
+app.use(cors({
+  origin: ['http://localhost:5173',],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
+}));
+
 // Middleware
-app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
