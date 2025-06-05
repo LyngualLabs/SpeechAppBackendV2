071990b14d39d5c1365b684a262dc43a4ba2ee07
RhythmicRhythm <okubadejorh@gmail.com>
2025-06-05 10:49:31 +0100

    Update CORS configuration to allow requests from the production domain

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:5173',],
+  origin: ['http://localhost:5173', "https://ambitious-mushroom-0397fd710.6.azurestaticapps.net"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
