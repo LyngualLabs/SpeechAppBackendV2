9f8a49771c12d47b34d7fbc38087c2fdd96793aa
RhythmicRhythm <okubadejorh@gmail.com>
2025-07-23 15:44:38 +0100

    feat: set default role to "user" in signUp and update UserSchema comment

--- src/models/User.ts
+++ src/models/User.ts
@@ -21,6 +21,7 @@ const UserSchema = new Schema<IUser>(
     role: {
       type: String,
       enum: ["super-admin", "admin", "user"],
+      // default: "user",
     },
     recordCounts: {
       totalRegular: { type: Number, default: 0 },
